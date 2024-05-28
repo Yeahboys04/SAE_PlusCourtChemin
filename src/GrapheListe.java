1009b99095fec1feabda8372ffecd58f0e1b6ff2
Ryan Korban <ryankorban2004@gmail.com>
2024-05-28 16:00:44 +0200

    Ajour de Graphe et GrapheListe

---
+++ src/GrapheListe.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class GrapheListe implements Graphe{
+    @Override
+    public List<String> listeNoeuds() {
+        return null;
+    }
+
+    @Override
+    public List<Arc> suivants(String n) {
+        return null;
+    }
+}
