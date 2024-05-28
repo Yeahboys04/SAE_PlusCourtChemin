dd4e69aba5f5d311a918430495115b267f12e4a8
Ryan Korban <ryankorban2004@gmail.com>
2024-05-28 16:11:20 +0200

    Ajout de GrapheListe + getIndice

--- src/GrapheListe.java
+++ src/GrapheListe.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class GrapheListe implements Graphe{
+    ArrayList<String> noeuds;
+    ArrayList<Arcs> adjacence;
+
     @Override
     public List<String> listeNoeuds() {
         return null;
@@ -9,5 +13,15 @@ public class GrapheListe implements Graphe{
     @Override
     public List<Arc> suivants(String n) {
         return null;
+    }
+
+    public int getIndice(String n){
+        int ind =0;
+        for(int i =0;i<this.noeuds.size();i++){
+            if(this.noeuds.get(i).compareTo(n)==0){
+                ind = i;
+            }
+        }
+        return ind;
     }
 }
