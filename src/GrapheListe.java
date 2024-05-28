1dc02008d1467099ccd2025979d3ceaa0f8ac499
Ryan Korban <ryankorban2004@gmail.com>
2024-05-28 16:29:44 +0200

    Ajout ajouterArc + modif getIndice

--- src/GrapheListe.java
+++ src/GrapheListe.java
@@ -1,13 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class GrapheListe implements Graphe{
+public class GrapheListe implements Graphe {
     ArrayList<String> noeuds;
     ArrayList<Arcs> adjacence;
 
     @Override
-    public List<String> listeNoeuds() {
-        return null;
+    public ArrayList<String> listeNoeuds() {
+        return noeuds;
     }
 
     @Override
@@ -15,13 +15,24 @@ public class GrapheListe implements Graphe{
         return null;
     }
 
-    public int getIndice(String n){
-        int ind =0;
-        for(int i =0;i<this.noeuds.size();i++){
-            if(this.noeuds.get(i).compareTo(n)==0){
+    public int getIndice(String n) {
+        int ind = -1;
+        for (int i = 0; i < this.noeuds.size(); i++) {
+            if (this.noeuds.get(i).compareTo(n) == 0) {
                 ind = i;
             }
         }
         return ind;
+    }
+
+    public void ajouterArc(String depart, String destination, double cout) {
+        int indArcDep = getIndice(depart);
+        int indArcDes = getIndice(destination);
+        if (indArcDes != -1 && indArcDep != -1) {
+            Arcs dep = this.adjacence.get(indArcDep);
+            Arc des = new Arc(destination, cout);
+            dep.ajouterArc(des);
+        }
+
     }
 }
