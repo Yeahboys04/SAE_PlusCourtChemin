eb86b868b9240a2d683bb2250964d3c6eb310c6f
Packingdustry <alois.mc@hotmail.com>
2024-05-31 11:24:20 +0200

    ajoute la javadoc de l'interface Graphe

--- src/Graphe.java
+++ src/Graphe.java
@@ -1,5 +1,8 @@
 import java.util.List;
 
+/**
+ * interface qui définit les méthodes nécessaires à l'implémentation d'un graphe
+ */
 public interface Graphe {
     public List<String> listeNoeuds();
     public List<Arc> suivants(String n);
