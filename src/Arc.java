6c3a93378fcd97c7ff655751eb7bd29ee24535cd
Packingdustry <alois.mc@hotmail.com>
2024-05-31 11:17:34 +0200

    ajoute la javadoc de Arc et Arcs

--- src/Arc.java
+++ src/Arc.java
@@ -1,16 +1,32 @@
+/**
+ * Classe représentant un arc allant jusqu'au sommet dest avec un coût de cout dans un graphe.
+ */
 public class Arc {
     private String dest;
     private double cout;
 
+    /**
+     * constructeur avec destination et cout
+     * @param dest String destination de l'arc
+     * @param cout double coût de l'arc
+     */
     public Arc(String dest, double cout){
         this.dest = dest;
         this.cout = cout;
     }
 
+    /**
+     * getter du coût
+     * @return double le coût de l'arc
+     */
     public double getCout() {
         return cout;
     }
 
+    /**
+     * getter de la destination
+     * @return String la destination de l'arc
+     */
     public String getDest() {
         return dest;
     }
--- src/Arcs.java
+++ src/Arcs.java
@@ -1,16 +1,30 @@
 import java.util.ArrayList;
 
+/**
+ * Classe représentant une liste d'arcs et la méthode pour y ajouter un nouvel arc.
+ */
 public class Arcs {
     private ArrayList<Arc> arcs;
 
+    /**
+     * constructeur par défaut
+     */
     public Arcs() {
         arcs = new ArrayList<>();
     }
 
+    /**
+     * Méthode qui permet d'ajouter un arc
+     * @param a Arc à ajouter
+     */
     public void ajouterArc(Arc a) {
         arcs.add(a);
     }
 
+    /**
+     * renvoie les arcs de la liste
+     * @return ArrayList<Arc> liste des arcs
+     */
     public ArrayList<Arc> getArcs() {
         return arcs;
     }
